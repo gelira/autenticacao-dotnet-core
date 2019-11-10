69cf81b4c9795a86ec4c7fe64b41a73591129422
Geraldo Eloi <geraldo.eloi11@gmail.com>
2019-11-10 00:50:38 -0300

    Conversão da chave em bytes

--- Helpers/SecretKeyConfig.cs
+++ Helpers/SecretKeyConfig.cs
@@ -1,7 +1,14 @@
+using System.Text;
+
 namespace Autenticacao.Helpers
 {
     public class SecretKeyConfig
     {
         public string SecretKey { get; set; }
+
+        public byte[] SecretKeyBytes 
+        {
+            get => Encoding.ASCII.GetBytes(SecretKey);
+        }
     }
 }
