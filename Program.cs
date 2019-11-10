e9da1a905be2186e5bfc7174efbaf8491663d214
Geraldo Eloi <geraldo.eloi11@gmail.com>
2019-11-10 00:27:41 -0300

    Rota padrão sem HTTPS

--- Program.cs
+++ Program.cs
@@ -20,7 +20,8 @@ namespace Autenticacao
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                        .UseUrls("http://localhost:5000");
                 });
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -31,13 +31,6 @@ namespace Autenticacao
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
